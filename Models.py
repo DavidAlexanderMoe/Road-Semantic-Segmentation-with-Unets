a2dcd7c3a21104dd194abc3a82725a095b6d7e02
DavidAlexanderMoe <Davidalexander100@hotmail.com>
2024-02-04 00:39:01 +0100

    Added metrics

--- Models.py
+++ Models.py
@@ -12,6 +12,20 @@ from keras.optimizers import Adam
 # Metrics
 from keras.metrics import MeanIoU
 
+############################################################################################################################################################################
+# METRICS
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2.0 * intersection + 1.0) / (K.sum(y_true_f) + K.sum(y_pred_f) + 1.0)
+
+
+def jacard_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (intersection + 1.0) / (K.sum(y_true_f) + K.sum(y_pred_f) - intersection + 1.0)
 
 ############################################################################################################################################################################
 
@@ -227,9 +241,6 @@ def attention_block():
 def ResUnet_att():
     pass
 
-
-# TRY TRASFER LEARNING WITH AUTOENCODER
-# CHANGE OPTIMIZERS AND CHECK FILTERS
 
 
 
